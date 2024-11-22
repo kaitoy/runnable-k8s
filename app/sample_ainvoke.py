5fbe9f536a8a1da83c6d35b1dc756d797ee1f815
kaitoy <kaitoy@pcap4j.org>
2024-11-22 18:05:41 +0900

    Add ainvoke sample.

---
+++ app/sample_ainvoke.py
@@ -0,0 +1,24 @@
+import asyncio
+import sys
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from runnables.runnable_k8s import RunnableK8s
+
+
+prompt = ChatPromptTemplate.from_messages([
+    (
+        "system",
+        "あなたはポケモンのエキスパートです。ユーザの質問に答えてください。",
+    ),
+    (
+        "human",
+        "質問: {input}",
+    )
+])
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+chain = prompt | RunnableK8s(bound=llm) | StrOutputParser()
+
+print(asyncio.run(chain.ainvoke({'input': sys.argv[1]})))
